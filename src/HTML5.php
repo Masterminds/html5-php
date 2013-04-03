97a89bd7189c31c34faa9109bf71aa59ac482727
Matt Butcher <technosophos@gmail.com>
2013-04-03 09:45:11 -0500

    Top-level HTML5 object.

---
+++ src/HTML5.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The main HTML5 front end.
+ *
+ * This class offers convenience methods for parsing and serializing HTML5.
+ */
+class HTML5 extends \HTML5\Parser {
+  // Inherit parse() and parseFragment().
+
+  /**
+   * Write the HTML5 to a string.
+   */
+  public static function write($dom) {
+  }
+}

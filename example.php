dc5934d59b82fd16bfed7237ede6f17f1dd542d1
Matt Butcher <technosophos@gmail.com>
2013-04-03 21:32:32 -0500

    Updated example to have better HTML.

--- example.php
+++ example.php
@@ -3,8 +3,20 @@
 require "vendor/autoload.php";
 
 
-$html = "<html><head><title>TEST</title></head><body>Hello World</body></html>";
+$html = <<< 'HERE'
+  <html><head><title>TEST</title></head>
+  <body id='foo'>
+  <!-- This space intentionally left blank. -->
+  <section class="section-a pretty" id="bar1">
+  <h1>Hello World</h1><p>This is a test of the HTML5 parser.</p>
+  <hr>
+  </section>
+  <![CDATA[Because we can.]]>
+  </body></html>
+HERE;
 
 $dom = \HTML5::parse($html);
 
-print_r($dom);
+print "Converting to HTML 5\n";
+
+\HTML5::save($dom, fopen("php://stdin", 'w'));

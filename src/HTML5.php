be997f20370f7227b05ec87a977645f0b09fd96c
Matt Butcher <technosophos@gmail.com>
2013-04-03 21:32:55 -0500

    Updated main HTML5 class to use serializer.

--- src/HTML5.php
+++ src/HTML5.php
@@ -4,13 +4,25 @@
  * The main HTML5 front end.
  *
  * This class offers convenience methods for parsing and serializing HTML5.
+ *
+ * EXPERIMENTAL. This may change or be completely replaced.
  */
 class HTML5 extends \HTML5\Parser {
   // Inherit parse() and parseFragment().
 
   /**
-   * Write the HTML5 to a string.
+   * Save a DOM into a given file as HTML5.
    */
-  public static function write($dom) {
+  public static function save($dom, $file) {
+    $serializer = new \HTML5\Serializer($dom);
+    return $serializer->save($file);
+  }
+
+  /**
+   * Convert a DOM into an HTML5 string.
+   */
+  public static function saveHTML($dom) {
+    $serializer = new \HTML5\Serializer($dom);
+    return $serializer->saveHTML();
   }
 }

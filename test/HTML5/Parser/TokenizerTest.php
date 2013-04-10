843732d17e0ce98573b1228df6b3ca75e95a4703
Technosophos <technosophos@gmail.com>
2013-04-10 11:49:52 -0500

    Added test and stubbed out first method.

--- test/HTML5/Parser/TokenizerTest.php
+++ test/HTML5/Parser/TokenizerTest.php
@@ -3,4 +3,21 @@ namespace HTML5\Parser;
 require __DIR__ . '/../TestCase.php';
 
 class TokenizerTest extends TestCase {
+  protected function createTokenizer($string) {
+    $eventHandler = new EventStack();
+    $stream = new StringInputStream($string);
+    $scanner = new Scanner($stream);
+    return array(
+      new Tokenizer($scanner, $eventHandler),
+      $eventHandler,
+    );
+  }
+
+  public function testParse() {
+    list($tok, $events) = $this->createTokenizer('');
+
+    $tok->parse();
+
+    $this->assertEquals(0, $events->Depth());
+  }
 }

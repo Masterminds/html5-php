bdd5822cda3df4c1ef6e7a1d66b844ff534491ce
Technosophos <technosophos@gmail.com>
2013-04-10 11:50:06 -0500

    Added EventStack testing event handler.

---
+++ test/HTML5/Parser/EventStack.php
@@ -0,0 +1,57 @@
+<?php
+namespace HTML5\Parser;
+require __DIR__ . '/../TestCase.php';
+
+class EventStack implements EventHandler {
+  protected $stack;
+
+  public function __construct() {
+    $this->stack = array();
+  }
+
+  /**
+   * Get the event stack.
+   */
+  public function events() {
+    return $this->stack;
+  }
+
+  public function depth() {
+    return count($this->stack);
+  }
+
+  protected function store($event, $data = NULL) {
+    $stack[] = array(
+      'name' => $event,
+      'data' => $data,
+    );
+  }
+
+  public function doctype($name, $publicId, $systemID, $quirks) {
+    $args = func_get_args();
+    $this->store('doctype', $args);
+  }
+
+  public function startTag($name, $attributes = array(), $selfClosing = FALSE) {
+    $args = func_get_args();
+    $this->store('startTag', $args);
+  }
+
+  public function endTag($name) {
+    $this->store('endTag', array($name));
+  }
+
+  public function comment($cdata) {
+    $this->store('comment', array($cdata));
+  }
+
+  public function text($cdata) {
+    $this->store('character', array($cdata));
+  }
+
+  public function eof() {
+    $this->store('eof');
+  }
+
+
+}

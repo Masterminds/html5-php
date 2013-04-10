ab9d8dbaf0469a1ab419f283e1e1e21fec1c1b74
Technosophos <technosophos@gmail.com>
2013-04-10 15:42:49 -0500

    Entity reference generator.

---
+++ bin/entities.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Fetch the entities.json file and convert to PHP datastructure.
+ */
+
+// The URL to the official entities JSON file.
+$ENTITIES_URL = 'http://www.w3.org/TR/2012/CR-html5-20121217/entities.json';
+
+$payload = file_get_contents($ENTITIES_URL);
+$json = json_decode($payload);
+
+$table = array();
+foreach ($json as $name => $obj) {
+  $sname = substr($name, 1, -1);
+  $table[$sname] = $obj->characters;
+}
+
+print '<?php
+$entities = ';
+var_export($table);
+print ';';
+//print serialize($table);

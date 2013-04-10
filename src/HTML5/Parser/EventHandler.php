3025f278b30d45242b41d3b009a3ee5c539bd015
Technosophos <technosophos@gmail.com>
2013-04-10 17:51:31 -0500

    Updated documentation in EventHandler.

--- src/HTML5/Parser/EventHandler.php
+++ src/HTML5/Parser/EventHandler.php
@@ -7,12 +7,36 @@ namespace HTML5\Parser;
  * See HTML5 spec section 8.2.4
  */
 interface EventHandler {
+  /**
+   * A doctype declaration.
+   */
   public function doctype($name, $publicID, $systemID, $quirks = FALSE);
+  /**
+   * A start tag.
+   */
   public function startTag($name, $attributes = array(), $selfClosing = FALSE);
+  /**
+   * An end-tag.
+   */
   public function endTag($name);
+  /**
+   * A comment section (unparsed character data).
+   */
   public function comment($cdata);
+  /**
+   * A unit of parsed character data.
+   *
+   * Entities in this text are *already decoded*.
+   */
   public function text($cdata);
+  /**
+   * Indicates that the document has been entirely processed.
+   */
   public function eof();
+  /**
+   * Emitted when the parser encounters an error condition.
+   */
+  public function parseError($msg, $line, $col);
 
   // Do we need...
   // public function cdata();

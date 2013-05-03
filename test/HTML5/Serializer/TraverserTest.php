166265a60a5c0903a72eeca09381369edd1f6341
Matt Farina <matt@mattfarina.com>
2013-05-03 13:00:18 -0400

    Added constructor test to the traverser.

--- test/HTML5/Serializer/TraverserTest.php
+++ test/HTML5/Serializer/TraverserTest.php
@@ -8,6 +8,19 @@ require_once __DIR__ . '/../TestCase.php';
 
 class TraverserTest extends \HTML5\Tests\TestCase {
 
+  // Dummy markup to parse then try to traverse. Note, not using any html5
+  // so we can use the old parser until ours is complete.
+  protected $markup = '<!doctype html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Test</title>
+      </head>
+      <body>
+        <p>This is a test.</p>
+      </body>
+    </html>';
+
   /**
    * Using reflection we make a protected method accessible for testing.
    * 
@@ -22,5 +35,21 @@ class TraverserTest extends \HTML5\Tests\TestCase {
     $method = $class->getMethod($name);
     $method->setAccessible(true);
     return $method;
+  }
+
+  function testConstruct() {
+
+    // The traverser needs a place to write the output to. In our case we
+    // use a stream in temp space.
+    $stream = fopen('php://temp', 'w');
+
+    // Using the existing parser (libxml).
+    // @todo switch to the html5 parser.
+    $dom = new \DOMDocument();
+    $dom->loadHTML($this->markup);
+
+    $t = new Traverser($dom, $stream);
+
+    $this->assertInstanceOf('\HTML5\Serializer\Traverser', $t);
   }
 }

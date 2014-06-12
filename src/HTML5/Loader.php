362ed7a9936cd53c7e120a9f02f146e8dacc32c6
Asmir Mustafic <goetas@gmail.com>
2014-06-12 15:47:13 +0200

    Split HTML5 class in two parts

--- src/HTML5/Loader.php
+++ src/HTML5/Loader.php
@@ -1,5 +1,5 @@
 <?php
-namespace Masterminds;
+namespace Masterminds\HTML5;
 
 use Masterminds\HTML5\Parser\FileInputStream;
 use Masterminds\HTML5\Parser\StringInputStream;
@@ -14,9 +14,8 @@ use Masterminds\HTML5\Serializer\Traverser;
  * It is roughly designed to mirror the \DOMDocument class that is
  * provided with most versions of PHP.
  *
- * EXPERIMENTAL. This may change or be completely replaced.
  */
-class HTML5
+class Loader
 {
 
     /**
@@ -155,7 +154,7 @@ class HTML5
      * Lower-level loading function. This requires an input stream instead
      * of a string, file, or resource.
      */
-    public function parse(\Masterminds\HTML5\Parser\InputStream $input)
+    protected function parse(\Masterminds\HTML5\Parser\InputStream $input)
     {
         $this->errors = array();
         $events = new DOMTreeBuilder();
@@ -179,7 +178,7 @@ class HTML5
      * Lower-level loading function. This requires an input stream instead
      * of a string, file, or resource.
      */
-    public function parseFragment(\Masterminds\HTML5\Parser\InputStream $input)
+    protected function parseFragment(\Masterminds\HTML5\Parser\InputStream $input)
     {
         $events = new DOMTreeBuilder(TRUE);
         $scanner = new Scanner($input);
@@ -188,59 +187,5 @@ class HTML5
         $parser->parse();
 
         return $events->fragment();
-    }
-
-    /**
-     * Save a DOM into a given file as HTML5.
-     *
-     * @param mixed $dom
-     *            The DOM to be serialized.
-     * @param string $file
-     *            The filename to be written.
-     * @param array $options
-     *            Configuration options when serializing the DOM. These include:
-     *            - encode_entities: Text written to the output is escaped by default and not all
-     *            entities are encoded. If this is set to TRUE all entities will be encoded.
-     *            Defaults to FALSE.
-     */
-    public function save($dom, $file, $options = array())
-    {
-        $close = TRUE;
-        if (is_resource($file)) {
-            $stream = $file;
-            $close = FALSE;
-        } else {
-            $stream = fopen($file, 'w');
-        }
-        $options = array_merge($this->getOptions(), $options);
-        $rules = new OutputRules($stream, $options);
-        $trav = new Traverser($dom, $stream, $rules, $options);
-
-        $trav->walk();
-
-        if ($close) {
-            fclose($stream);
-        }
-    }
-
-    /**
-     * Convert a DOM into an HTML5 string.
-     *
-     * @param mixed $dom
-     *            The DOM to be serialized.
-     * @param array $options
-     *            Configuration options when serializing the DOM. These include:
-     *            - encode_entities: Text written to the output is escaped by default and not all
-     *            entities are encoded. If this is set to TRUE all entities will be encoded.
-     *            Defaults to FALSE.
-     *
-     * @return string A HTML5 documented generated from the DOM.
-     */
-    public function saveHTML($dom, $options = array())
-    {
-        $stream = fopen('php://temp', 'w');
-        $this->save($dom, $stream, array_merge($this->getOptions(), $options));
-
-        return stream_get_contents($stream, - 1, 0);
     }
 }

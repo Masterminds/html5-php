7c9583477c1f6a85016fb1c3a8339128ee4a8811
Matt Farina <matt@mattfarina.com>
2015-06-23 10:48:04 -0400

    Setting variable for local namespaces to protected in writer.
    The namespaces for mathml, html, and svg are set to protected
    so they cannot be altered. These are part of the spec.

--- src/Html5/Serializer/Traverser.php
+++ src/Html5/Serializer/Traverser.php
@@ -16,7 +16,7 @@ class Traverser
     /**
      * Namespaces that should be treated as "local" to HTML5.
      */
-    static $local_ns = array(
+    protected static $local_ns = array(
         'http://www.w3.org/1999/xhtml' => 'html',
         'http://www.w3.org/1998/Math/MathML' => 'math',
         'http://www.w3.org/2000/svg' => 'svg'

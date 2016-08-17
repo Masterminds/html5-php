7dbd2ada393f65994e1b3aae2754e09a057b3291
Danny Smart <dannysmart@silktide.com>
2016-08-17 18:19:26 +0100

    When outputting a TEXT_RAW node, make sure each child is an instance of DOMCharacterData

--- src/HTML5/Serializer/OutputRules.php
+++ src/HTML5/Serializer/OutputRules.php
@@ -221,7 +221,9 @@ class OutputRules implements \Masterminds\HTML5\Serializer\RulesInterface
         $this->openTag($ele);
         if (Elements::isA($name, Elements::TEXT_RAW)) {
             foreach ($ele->childNodes as $child) {
-                $this->wr($child->data);
+                if ($child instanceof \DOMCharacterData) {
+                    $this->wr($child->data);
+                }
             }
         } else {
             // Handle children.
